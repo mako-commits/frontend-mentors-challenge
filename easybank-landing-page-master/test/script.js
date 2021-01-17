52de0caa9b1fe5ac2b3543b9655f5d012f4cccb6
pythagoras-dev <mubarakmustapha15@gmail.com>
2021-01-17 14:55:41 +0100

    all done for easybank landing page

---
+++ easybank-landing-page-master/test/script.js
@@ -0,0 +1,27 @@
+(function () {
+
+    "use strict";
+
+
+
+
+
+    //===== close navbar-collapse when a  clicked
+    let navbarToggler = document.querySelector(".navbar-toggler");
+    var navbarCollapse = document.querySelector(".navbar-collapse");
+
+    document.querySelectorAll(".page-scroll").forEach(e =>
+        e.addEventListener("click", () => {
+            navbarToggler.classList.remove("active");
+            navbarCollapse.classList.remove('show')
+        })
+    );
+    navbarToggler.addEventListener('click', function () {
+        navbarToggler.classList.toggle("active");
+    });
+
+
+
+
+
+})();
