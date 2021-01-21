3a024b78b64de06a593fa4542207ee0c4244e215
pythagoras-dev <mubarakmustapha15@gmail.com>
2021-01-21 15:22:07 +0100

    done i guess

--- bookmark-landing-page-master/assets/js/script.js
+++ bookmark-landing-page-master/assets/js/script.js
@@ -82,3 +82,22 @@ function removeShow() {
 }
 //listen for tab click
 tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+
+
+// faq
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+    const itemToggle = this.getAttribute('aria-expanded');
+
+    for (i = 0; i < items.length; i++) {
+        items[i].setAttribute('aria-expanded', 'false');
+    }
+
+    if (itemToggle == 'false') {
+        this.setAttribute('aria-expanded', 'true');
+    }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
